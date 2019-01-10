54065d08e65891dffe2605a4bf578c4a6e1faec3
William Saffran <willh.saffran@gmail.com>
2019-01-10 20:26:18 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,12 @@
+function outerFunction() {
+  var innerVariable = "I'm sort of a secret.";
+ 
+  return function innerScope() {
+    var inaccessible = "Nothing can touch me.";
+ 
+    return innerVariable;
+  }
+}
+
+var myScope = outerFunction();
+myScope;
