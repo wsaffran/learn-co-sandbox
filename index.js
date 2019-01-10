da04076476ca5d336cff27bffcc1b52ad10efb7f
William Saffran <willh.saffran@gmail.com>
2019-01-10 20:30:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const person =
   {name: "Awesome Name", 
   occupation: {
     title: "Senior Manager of Awesome", 
-  yearsHeld: 2},
-  pets: [{kind: "dog", name: "Fiona"},
-  {kind: "cat", name: "Ralph"}]
-  
+    yearsHeld: 2},
+  pets: [{
+    kind: "dog", name: "Fiona"}, {
+    kind: "cat", name: "Ralph"}]
+}
